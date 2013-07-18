9b9bb0ee0c9851aaee70d95f55c9832a10cfc51a
Iararibeiro <Iara.prbr@gmail.com>
2013-07-18 10:44:20 -0300

    Proessamento dos  NA's
    Tambem organizei os scripts e os resultados por pastas.

---
+++ US02/RScripts/porcentagem_na_maquinas.R
@@ -0,0 +1,16 @@
+dados <- read.csv("rua-analytics/short_duration/xsys.csv")
+
+porc_maquina = c()
+
+for(i in 4:ncol(dados)){
+  
+  num_complete = length(na.omit(dados[,i]))
+  num_na = length(dados[,i]) - num_complete 
+  porc_maquina = rbind(porc_maquina, cbind(num_na/num_complete))
+}
+
+porc_maquina = as.data.frame(porc_maquina)
+rownames(porc_maquina) = colnames(dados)[4:ncol(dados)]
+colnames(porc_maquina) = c("porcentagem")
+
+write.csv(porc_maquina, "porcentagem_na_maquina.csv")
