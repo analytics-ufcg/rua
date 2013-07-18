c19f22f0f27edcd767978f0e6c13bc81144fb96a
Alberto Fagner <albertofagner.cav@gmail.com>
2013-07-18 15:53:32 -0300

    processamento dos 100

--- US02/RScripts/processamento 100.R
+++ US02/RScripts/processamento 100.R
@@ -2,14 +2,21 @@
 #ncol nrow
 
 dados <- read.csv("xsys.csv")
+tamanho <- nrow(dados)
+df <- list()
 
-encontra.limite.superior<- function(inicio, fim, limite)
-for(contador in c(inicio:fim)){
-  if(max(dados[contador], na.rm=TRUE)>=limite){
-    temp <- subset(dados, dados[contador]>=limite, select=c(names(dados[1]), names(dados[2]), names(dados[3]), names(dados[contador])))
-    nome = paste(paste("coluna", contador, sep=""),".csv", sep="")
-    write.csv(temp, nome)
+encontra.limite.superior<- function(inicio, fim, limite){
+  for(contador in c(inicio:fim)){
+    if(max(dados[contador], na.rm=TRUE)>=limite){
+      temp <- subset(dados, dados[contador]>=limite, select=c(names(dados[1]), names(dados[2]), names(dados[3]), names(dados[contador])))
+      nome = paste(paste("coluna", contador, sep=""),".csv", sep="")
+      write.csv(temp, nome)
+      lista<-c(names(dados[contador]),((nrow(temp)/tamanho)*100))
+      df<-rbind(df,lista)
   }
+ }
+  colnames(df) = c("Maquina", "Porcentagem")
+  write.csv(df, "Porcentagem_dos_100.csv", row.names=F, quote=F)
 }
 
 encontra.limite.superior(4,393,100)
