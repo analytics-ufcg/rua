c2abb0c7f1678f84de591dd1c7c3066951af7649
Iara Ribeiro <Iara.prbr@gmail.com>
2013-07-26 13:17:50 -0300

    Script que seleciona os traces da us06

---
+++ US06/SelectTraces.R
@@ -0,0 +1,19 @@
+traces_selecionados <- read.csv("Maquinas_selecionas.csv", head = T)
+dados <- read.csv("xsys.csv", head = T)
+
+dados_selecionados = dados[,1:3]
+
+for(i in seq(4:393)){
+  maquina = c()
+  if(is.element(colnames(dados)[i], traces_selecionados[,1]) == TRUE){
+    print(colnames(dados)[i])
+    print(traces_selecionados[,1])
+    print(is.element(colnames(dados)[i], traces_selecionados[,1]))
+    dados_selecionados = cbind(dados_selecionados, dados[,i])
+  }
+}
+
+colnames(dados_selecionados) = c("x","data","time","M105", "M121", "M122", "M137", "M143", "M169", "M173", "M174", "M178", "M196", "M243",
+                                 "M300", "M308", "M376", "M379", "M86",  "M87",  "M9",   "M93",  "M97")
+
+write.csv(dados_selecionados,"traces_selecionados.csv")
