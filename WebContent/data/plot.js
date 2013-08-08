366924221cc82bbff06b3262c9bffff0b2880e0d
Iara Ribeiro <Iara.prbr@gmail.com>
2013-08-08 17:57:05 -0300

    Esqueleto do site

--- WebContent/data/plot.js
+++ WebContent/data/plot.js
@@ -17,14 +17,25 @@ function cleanContainers(){
 function loadData(){
 	
 	d3.csv("data/Maquinas_selecionas.csv", function(data){
-		
 		var myList = d3.selectAll("#myList");
 		myList.selectAll("option").data(data).enter().append("option")
 		.attr("value",function(d){return d.Maquina;})
 		.attr("label",function(d){return d.Maquina;})
 		.text(function(d){return d.Maquina;});
-		console.log(myList);
+		
+		dataset = data;
 	});
-	
-	
+
+}
+
+function filtrar_aplicacoes(){
+	console.log(dataset);
+}
+
+function navegar_traces(){
+	console.log("Navegar entre os traces");
+}
+
+function classificar_aplicacoes(){
+	console.log("Classificar aplicacoes");
 }
