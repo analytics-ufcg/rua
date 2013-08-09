c3bc3d70c97fb03bbed322305d6b93046d094693
Iararibeiro <Iara.prbr@gmail.com>
2013-08-09 11:58:26 -0300

    Atualização do site

--- WebContent/data/plot.js
+++ WebContent/data/plot.js
@@ -2,6 +2,7 @@
 var filtro_mem = [];
 var filtro_cpu = [];
 var maquina;
+var opcao_caract;
 
 function getMenuOption(selection) {
 	//limpa containers
@@ -33,6 +34,7 @@ function filtrar_aplicacoes(opcao){
 	filtro_mem = dataset.filter(function(i){return i.mem == "TRUE";});
 	filtro_cpu = dataset.filter(function(i){return i.cpu == "TRUE";});
 	var myList = d3.selectAll("#myList");
+	opcao_carac = opcao;
 	
 	//Atualizar dropdown a cada selecao de variavel
 	if(opcao == "1"){
@@ -68,7 +70,8 @@ function filtrar_aplicacoes(opcao){
 
 function getAplicacao(){
     var img;
-    img = document.createElement('img');
+    var svg;
+	img = document.createElement('img');
     img.src = "data/graficos/mem/" + maquina + ".png";
     img.style.width  = 940  + "px";
     img.style.height = 300 + "px";
@@ -77,16 +80,47 @@ function getAplicacao(){
 	img.style.left = "250px";
     target = document.getElementById("caracteristicas");
     target.appendChild(img);
-
+	changeText();
 }
 
 function clearImages(){
 	if(document.getElementById("img") != null){
 		document.getElementById("img").remove();
-		d3.selectAll("svg").remove();
 	}
 }
 
+function getvalores_mem(variavel){
+	if(variavel =="1"){
+		return filtro_mem.filter(function(i){return i.maquina == maquina;})[4];
+	}
+}
+
+function changeText(){
+	var maquina_selecionada,texto;
+	if(opcao_carac == "1"){
+		maquina_selecionada = filtro_mem.filter(function(i){return i.maquina == maquina;});
+		texto ="Variabilidade: "+ maquina_selecionada[0].var_mem +
+		"<br>Desvio Padrão: "+ maquina_selecionada[0].sd_mem +
+		"<br>Sazonalidade: "+ maquina_selecionada[0].sazonalidade_mem +
+		"<br>Tendência: "+ maquina_selecionada[0].tendencia_mem +
+		"<br>Grau de autocor.:" + maquina_selecionada[0].grauautocor_mem;
+	}else if(opcao_carac =="2"){
+		maquina_selecionada = filtro_cpu.filter(function(i){return i.maquina == maquina;});
+		texto ="Variabilidade: "+ maquina_selecionada[0].var_cpu +
+		"<br>Desvio Padrão: "+ maquina_selecionada[0].sd_cpu +
+		"<br>Sazonalidade: "+ maquina_selecionada[0].sazonalidade_cpu +
+		"<br>Tendência: "+ maquina_selecionada[0].tendencia_cpu +
+		"<br>Grau de autocor.:" + maquina_selecionada[0].grauautocor_cpu;
+	}else if(opcao_carac =="3"){
+		maquina_selecionada = filtro_mem.filter(function(i){return i.maquina == maquina;});
+		texto ="Variabilidade: "+ maquina_selecionada[0].var_mem + " , " + maquina_selecionada[0].var_cpu +
+		"<br>Desvio Padrão: "+ maquina_selecionada[0].sd_mem + ", " + maquina_selecionada[0].sd_cpu +
+		"<br>Sazonalidade: "+ maquina_selecionada[0].sazonalidade_mem + ", " + maquina_selecionada[0].sazonalidade_cpu +
+		"<br>Tendência: "+ maquina_selecionada[0].tendencia_mem + ", " + maquina_selecionada[0].tendencia_cpu +
+		"<br>Grau de autocor.:" + maquina_selecionada[0].grauautocor_mem; ", " + maquina_selecionada[0].grauautocor_cpu;
+	}
+	document.getElementById('legenda').innerHTML = "<br><br><br><br><br>" + texto;
+}
 
 // function navegar_traces(){
 	// console.log("Navegar entre os traces");
