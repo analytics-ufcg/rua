186d909cf363057c9870ad1471d278ae2d53edaa
Alberto Fagner <albertofagner.cav@gmail.com>
2013-08-29 14:23:12 -0300

    ultima versão

--- US13/clusterAcf.R
+++ US13/clusterAcf.R
@@ -1,23 +1,41 @@
-dados <- read.csv("filesInput/xsys4.csv")
-dados$CPU_UTIL <- ceiling(dados$CPU_UTIL)
-serieA <- subset(dados, select = CPU_UTIL)
+clusterAcf <- function(){
 
-dados <- read.csv("filesInput/xsys5.csv")
-dados$CPU_UTIL <- ceiling(dados$CPU_UTIL)
-serieB <- subset(dados, select = CPU_UTIL)
+	fileNames <- list.files(pattern="*.csv")
 
-serieA = acf(serieA, lag.max = nrow(serieA), plot = FALSE, na.action = na.pass)
-serieB = acf(serieB, lag.max = nrow(serieB), plot = FALSE, na.action = na.pass)
+	matriz <- dist(c(1:length(fileNames)), method = "euclidean")
+	contador = 1
 
-serieA = serieA$acf
-serieB = serieB$acf
+	for(i in 1:(length(fileNames)-1)){
 
-if(length(serieA)>length(serieB)){
-    dif = serieA[1:length(serieB)] - serieB
-}else{
-    dif = serieA - serieB[1:length(serieA)]
+		dados <- read.csv(fileNames[i])
+		dados$CPU_UTIL <- ceiling(dados$CPU_UTIL)
+		serieA <- subset(dados, select = CPU_UTIL)
+		serieA = acf(serieA, lag.max = nrow(serieA)/2, plot = FALSE, na.action = na.pass)
+		serieA = serieA$acf
+
+		for(j in (i+1):length(fileNames)){
+
+			dados <- read.csv(fileNames[j])
+			dados$CPU_UTIL <- ceiling(dados$CPU_UTIL)
+			serieB <- subset(dados, select = CPU_UTIL)
+			serieB = acf(serieB, lag.max = nrow(serieB)/2, plot = FALSE, na.action = na.pass)
+			serieB = serieB$acf
+
+			if(length(serieA)>length(serieB)){
+				dif = serieA[1:length(serieB)] - serieB
+			}else{
+				dif = serieA - serieB[1:length(serieA)]
+			}
+
+			matriz[contador][1] = sum(abs(dif))
+			contador = contador + 1
+		}
+
+	}
+
+	matriz
 }
 
-sum(dif)
+clusterAcf()
 
 dbscan(matriz, eps = 1, MinPts = 1)
